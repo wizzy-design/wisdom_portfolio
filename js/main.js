f5b7ad150373fde4f0e5625008bcf77363a2b6be
Wisdom Ochei <wisdomofgod2003@gmail.com>
2022-07-28 10:48:05 +0100

    My portfolio site (Demo)
    Still need to finish up some functions and images, this will do for now.

--- js/main.js
+++ js/main.js
@@ -4,41 +4,43 @@ const navMenu = document.getElementById('nav-menu'),
     closeMenu = document.getElementById('nav-close');
 
 // Show
-toggleMenu.addEventListener("click", ()=> {
-    navMenu.classList.toggle('show');
+toggleMenu.addEventListener('click', ()=> {
+    navMenu.classList.toggle('show')
 })
 
 // Hidden
-closeMenu.addEventListener("click", ()=> {
-    navMenu.classList.remove('show');
+closeMenu.addEventListener('click', ()=> {
+    navMenu.classList.remove('show')
 })
 
 // Remove Menu
-const navLink = document.querySelectorAll(".nav_link");
+const navLink = document.querySelectorAll('.nav_link')
 
 linkAction = () => {
-    navMenu.classList.remove('show');
+    navMenu.classList.remove('show')
 }
 
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach(n => n.addEventListener('click', linkAction))
 
 // Scroll sections active link
-const sections = document.querySelectorAll('section[id]');
+const sections = document.querySelectorAll('section[id]')
 
-windows.addEventListener('scroll', scrollActive);
-
-scrollActive = () => {
-    const scrollY = window.pageYOffset;
+const scrollActive = () => {
+    const scrollY = window.pageYOffset
 
     sections.forEach(current => {
-        const sectionHeight = current.offsetHeight;
-        const sectionTop = current.offsetTop - 50;
-        sectionId = current.getAttribute('id');
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50
+        sectionId = current.getAttribute('id')
 
-        if(scrollY > sectionTop && scrollY <= sectionTop +sectionHeight){
-            document.querySelector('.nav_menu a[href*='+ sectionId +']').classList.add('active');
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.nav_menu a[href*='+ sectionId +']').classList.add('active')
         }else{
-            document.querySelector('.nav_menu a[href*='+ sectionId +']').classList.remove('active');
+            document.querySelector('.nav_menu a[href*='+ sectionId +']').classList.remove('active')
         }
     })
 }
+
+window.addEventListener('scroll', scrollActive)
+
+
